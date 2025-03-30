79c7697f228d0dfc25878da11e6ddb28e2f41d5f
AnikaTabassum217 <tabassum.anika579@gmail.com>
2025-03-30 11:52:11 +0600

    Visited button working

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Country.css'
 const Country = ({country}) => {
-    console.log(country.flags.png)
+    // console.log(country.flags.png)
+
+
+    const [visited,setVisited] = useState(false)
 
     const handleVisited=()=>{
-         console.log('Visited')
+        //  console.log('Visited')
+        // setVisited(!visited)
+
+        if(visited===true){
+            setVisited(false)
+        }
+        else{
+            setVisited(true)
+        }
     }
 
     return (
-        <div className='country'>
-             
+        <div className={`country ${visited && 'country-visited'}`}>
+        {/* <div className={`country ${visited?'country-visited':''}`}>   */}
             <p>Name:{country.name.common}</p>
             <img width={200} height={200} src={country?.flags?.svg}></img>
             <p>independent: {country.independent===true?'Free':'Not Free'}</p>
             <p>Population:{country.population}</p>
-            <button onClick={handleVisited}>Not Visited</button>
+            <button className={visited? 'btn-visited':'btn-not-visited'} 
+            onClick={handleVisited}>{visited?'Visited':'Not visited'}</button>
         </div>
     );
 };
